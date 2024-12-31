2bb46422d39cd6bc5f8edc2a224f47909c275093
Evgeny A. Maltsev <yevmal@gmail.com>
2024-12-31 21:52:07 +0300

    Added the main module heictojpeg.py

---
+++ heictojpeg.py
@@ -0,0 +1,5 @@
+program_description = "HeicToJpeg by Evgeny A. Maltsev (yevmal@gmail.com)"
+program_version = "0.1.0"
+
+if __name__ == '__main__':
+    print(f'{program_description}\nversion {program_version}\n')
